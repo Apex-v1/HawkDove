2b68b485298b6aeb357edf2f055e9b037801a628
Apex-v1 <jrojasvenzor@gmail.com>
2026-04-09 23:10:13 -0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        mono: ['var(--font-mono)', 'monospace'],
-        display: ['var(--font-display)', 'sans-serif'],
-      },
-    },
-  },
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  theme: { extend: {} },
   plugins: [],
 }
